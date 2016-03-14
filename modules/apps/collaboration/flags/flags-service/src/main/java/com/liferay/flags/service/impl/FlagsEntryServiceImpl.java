92f3066d232dfcfdae90af0212fe2e62345f4dca
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2016-03-14 20:27:55 +0000

    LPS-63259 Rate limit FlagsEntryServiceImpl#addEntry() by content className + classPK

--- modules/apps/collaboration/flags/flags-service/src/main/java/com/liferay/flags/service/impl/FlagsEntryServiceImpl.java
+++ modules/apps/collaboration/flags/flags-service/src/main/java/com/liferay/flags/service/impl/FlagsEntryServiceImpl.java
@@ -18,10 +18,17 @@ import com.liferay.flags.messaging.FlagsRequest;
 import com.liferay.flags.service.base.FlagsEntryServiceBaseImpl;
 import com.liferay.portal.kernel.exception.EmailAddressException;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.RateLimitExceededException;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.MessageBusUtil;
+import com.liferay.portal.kernel.model.Ticket;
+import com.liferay.portal.kernel.model.TicketConstants;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.TicketLocalService;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.spring.extender.service.ServiceReference;
+
+import java.util.Date;
 
 /**
  * @author Julio Camarero
@@ -39,11 +46,46 @@ public class FlagsEntryServiceImpl extends FlagsEntryServiceBaseImpl {
 			throw new EmailAddressException();
 		}
 
+		checkRateLimiting(className, classPK, serviceContext);
+
 		FlagsRequest flagsRequest = new FlagsRequest(
 			className, classPK, reporterEmailAddress, reportedUserId,
 			contentTitle, contentURL, reason, serviceContext);
 
 		MessageBusUtil.sendMessage(DestinationNames.FLAGS, flagsRequest);
 	}
+
+	protected void checkRateLimiting(
+			String className, long classPK, ServiceContext serviceContext)
+		throws PortalException {
+
+		long companyId = serviceContext.getCompanyId();
+
+		Ticket throttleTicket = _ticketLocalService.fetchTicket(
+			className, classPK, TicketConstants.TYPE_FLAG);
+
+		if (Validator.isNotNull(throttleTicket) &&
+			!throttleTicket.isExpired()) {
+
+			throw new RateLimitExceededException(
+				"{className=\"" + className + "\"," + classPK + "," +
+					TicketConstants.TYPE_FLAG + "}");
+		}
+		else {
+			Date expirationDate = new Date(
+				System.currentTimeMillis() + getFlagRateLimit(companyId));
+
+			throttleTicket = _ticketLocalService.addDistinctTicket(
+				companyId, className, classPK, TicketConstants.TYPE_FLAG, null,
+				expirationDate, serviceContext);
+		}
+	}
+
+	private long getFlagRateLimit(long companyId) {
+		return 60000;
+	}
+
+	@ServiceReference(type = TicketLocalService.class)
+	private transient TicketLocalService _ticketLocalService;
 
 }
