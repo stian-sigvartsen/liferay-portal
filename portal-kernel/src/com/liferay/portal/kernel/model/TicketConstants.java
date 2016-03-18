4e554e3ebb979b3430772f05b1a68bcad0e41de6
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2016-03-18 22:06:27 +0000

    LPS-63259 New TicketConstants type for Flags

--- portal-kernel/src/com/liferay/portal/kernel/model/TicketConstants.java
+++ portal-kernel/src/com/liferay/portal/kernel/model/TicketConstants.java
@@ -22,6 +22,8 @@ public class TicketConstants {
 
 	public static final int TYPE_EMAIL_ADDRESS = 1;
 
+	public static final int TYPE_FLAG = 4;
+
 	public static final int TYPE_IMPERSONATE = 2;
 
 	public static final int TYPE_PASSWORD = 3;
