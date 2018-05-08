f0b2e5834b4af7d3568818947089851190a7f935
Carlos Sierra <carlos.sierra@liferay.com>
2018-05-08 12:08:10 +0200

    Bring oauth samples

---
+++ modules/apps/oauth2-provider/oauth2-provider-scope-sample-users/src/main/java/com/liferay/oauth2/provider/sample2/oauth/SampleUsersApp.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.sample2.oauth;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializer;
+import com.liferay.oauth2.provider.scope.RequiresNoScope;
+import com.liferay.oauth2.provider.scope.RequiresScope;
+import com.liferay.oauth2.provider.scope.liferay.LiferayOAuth2Scope;
+
+import java.util.Collection;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+
+import com.liferay.oauth2.provider.scope.liferay.ScopeLocator;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import org.osgi.framework.Bundle;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+@ApplicationPath("/sampleusers")
+@Component(
+	immediate = true,
+	property = {
+		"oauth2.scopechecker.type=annotations",
+		"osgi.jaxrs.name=liferay-oauth2-sample-users-app",
+		"requires.oauth2=true"
+	},
+	service = Application.class
+
+)
+public class SampleUsersApp extends Application {
+
+	@Override
+	public Set<Object> getSingletons() {
+		return Collections.singleton(this);
+	}
+
+	@GET
+	@RequiresScope({
+		"readuserinformation","readuseridentification","readorganizations",
+		"readconfigurations", "readpermissions"})
+	public String hello() {
+		return "hello";
+	}
+
+	@Reference
+	private ScopeLocator _scopeFinderLocator;
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+}
