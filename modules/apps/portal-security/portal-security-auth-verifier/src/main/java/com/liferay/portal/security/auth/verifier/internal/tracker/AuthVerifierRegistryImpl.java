ef599c3f6ed0d830f95a0dc9584b80dcfcbcbcac
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2019-09-11 20:02:18 +0100

    LPS-X SF

--- modules/apps/portal-security/portal-security-auth-verifier/src/main/java/com/liferay/portal/security/auth/verifier/internal/tracker/AuthVerifierRegistryImpl.java
+++ modules/apps/portal-security/portal-security-auth-verifier/src/main/java/com/liferay/portal/security/auth/verifier/internal/tracker/AuthVerifierRegistryImpl.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.portal.security.auth.verifier.internal.tracker;
 
 import com.liferay.osgi.util.ServiceTrackerFactory;
@@ -39,7 +53,10 @@ import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 import org.osgi.util.tracker.ServiceTracker;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
-@Component
+/**
+ * @author Stian Sigvartsen
+ */
+@Component(service = AuthVerifierRegistry.class)
 public class AuthVerifierRegistryImpl implements AuthVerifierRegistry {
 
 	public List<AuthVerifierConfiguration> getAuthVerifierConfigurations(
