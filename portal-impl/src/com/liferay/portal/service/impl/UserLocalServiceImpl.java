501d6a58beceffd6aa028f492b9976a95171cef0
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2019-09-12 16:19:59 +0100

    LPS-99587 For consistency the end user & admin password changing processes should generate a digest also

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -4851,7 +4851,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 			user.setPasswordModifiedDate(new Date());
 		}
 
-		user.setDigest(StringPool.BLANK);
+		user.setDigest(user.getDigest(password1));
 		user.setGraceLoginCount(0);
 
 		if (!silentUpdate) {
