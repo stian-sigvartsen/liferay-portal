f8a3dbb69ed5987dff73f5f8283f4b2a441ad453
Brian Chan <brian.chan@liferay.com>
2020-01-23 15:55:42 -0800

    LPS-105162 Simplify

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/WebSsoProfileImpl.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/WebSsoProfileImpl.java
@@ -753,10 +753,6 @@ public class WebSsoProfileImpl extends BaseProfile implements WebSsoProfile {
 		ServiceContext serviceContext = ServiceContextFactory.getInstance(
 			httpServletRequest);
 
-		ThemeDisplay themeDisplay =
-			(ThemeDisplay)httpServletRequest.getAttribute(
-				WebKeys.THEME_DISPLAY);
-
 		try {
 			User user = _userResolver.resolveUser(
 				new UserResolverSAMLContextImpl(messageContext),
@@ -796,8 +792,7 @@ public class WebSsoProfileImpl extends BaseProfile implements WebSsoProfile {
 				SamlWebKeys.SAML_SUBJECT_SCREEN_NAME, nameID.getValue());
 
 			httpServletResponse.sendRedirect(
-				getAuthRedirectURL(
-					messageContext, httpServletRequest, themeDisplay));
+				getAuthRedirectURL(messageContext, httpServletRequest));
 
 			return;
 		}
@@ -842,8 +837,7 @@ public class WebSsoProfileImpl extends BaseProfile implements WebSsoProfile {
 			samlSpSession.getSamlSpSessionKey(), -1);
 
 		httpServletResponse.sendRedirect(
-			getAuthRedirectURL(
-				messageContext, httpServletRequest, themeDisplay));
+			getAuthRedirectURL(messageContext, httpServletRequest));
 	}
 
 	protected void doSendAuthnRequest(
@@ -964,10 +958,14 @@ public class WebSsoProfileImpl extends BaseProfile implements WebSsoProfile {
 
 	protected String getAuthRedirectURL(
 			MessageContext<?> messageContext,
-			HttpServletRequest httpServletRequest, ThemeDisplay themeDisplay)
+			HttpServletRequest httpServletRequest)
 		throws PortalException {
 
 		StringBundler sb = new StringBundler(3);
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
 
 		sb.append(themeDisplay.getPathMain());
 
