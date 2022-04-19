19fb81fac0f54c7f47d5e7f9c7bad17c203e49b2
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2022-04-19 19:25:15 +0100

    LPS-120925 Marshall object

--- modules/dxp/apps/saml/saml-admin-rest-impl/src/main/java/com/liferay/saml/admin/rest/internal/resource/v1_0/IdpConnectionResourceImpl.java
+++ modules/dxp/apps/saml/saml-admin-rest-impl/src/main/java/com/liferay/saml/admin/rest/internal/resource/v1_0/IdpConnectionResourceImpl.java
@@ -14,9 +14,14 @@
 
 package com.liferay.saml.admin.rest.internal.resource.v1_0;
 
+import com.liferay.saml.admin.rest.dto.v1_0.IdpConnection;
 import com.liferay.saml.admin.rest.resource.v1_0.IdpConnectionResource;
 
+import com.liferay.saml.persistence.model.SamlSpIdpConnection;
+import com.liferay.saml.persistence.service.SamlSpAuthRequestLocalService;
+import com.liferay.saml.persistence.service.SamlSpIdpConnectionLocalService;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +32,41 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = IdpConnectionResource.class
 )
 public class IdpConnectionResourceImpl extends BaseIdpConnectionResourceImpl {
+
+	@Override
+	public IdpConnection getIdpConnection(
+		Long idpConnectionId) throws Exception {
+
+		SamlSpIdpConnection samlSpIdpConnection =
+			_samlSpIdpConnectionLocalService.getSamlSpIdpConnection(
+				idpConnectionId);
+
+		IdpConnection idpConnection = new IdpConnection();
+
+		idpConnection.setClockSkew(samlSpIdpConnection::getClockSkew);
+		idpConnection.setAssertionSignatureRequired(
+			samlSpIdpConnection::getAssertionSignatureRequired);
+		idpConnection.setForceAuthn(samlSpIdpConnection::getForceAuthn);
+		return new IdpConnection() {
+			{
+				//enabled = samlSpIdpConnection.getEnabled();
+				//entityId = samlSpIdpConnection.getSamlIdpEntityId();
+				//id = samlSpIdpConnection.getSamlSpIdpConnectionId();
+				//samlSpIdpConnection.getMetadataUpdatedDate();
+				//metadataUrl = samlSpIdpConnection.getMetadataUrl();
+				//name = samlSpIdpConnection.getName();
+				//nameIdFormat = samlSpIdpConnection.getNameIdFormat();
+
+				assertionSignatureRequired = samlSpIdpConnection.getAssertionSignatureRequired();
+				clockSkew = samlSpIdpConnection.getClockSkew();
+				forceAuthn = samlSpIdpConnection.getForceAuthn();
+				signAuthnRequest = samlSpIdpConnection.getSignAuthnRequest();
+				unknownUsersAreStrangers = samlSpIdpConnection.getUnknownUsersAreStrangers();
+				userAttributeMappings = samlSpIdpConnection.getUserAttributeMappings();
+			}
+		};
+	}
+
+	@Reference
+	private SamlSpIdpConnectionLocalService _samlSpIdpConnectionLocalService;
 }
