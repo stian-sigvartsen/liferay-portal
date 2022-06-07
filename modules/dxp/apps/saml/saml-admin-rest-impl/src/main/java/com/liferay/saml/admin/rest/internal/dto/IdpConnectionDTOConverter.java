22350ba578db463bf96cc60b6a73bd652c43102b
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2022-06-07 17:19:51 +0100

    LPS-120925 Fully implemented IdpConnectionDTOConverter

--- modules/dxp/apps/saml/saml-admin-rest-impl/src/main/java/com/liferay/saml/admin/rest/internal/dto/IdpConnectionDTOConverter.java
+++ modules/dxp/apps/saml/saml-admin-rest-impl/src/main/java/com/liferay/saml/admin/rest/internal/dto/IdpConnectionDTOConverter.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
 package com.liferay.saml.admin.rest.internal.dto;
 
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
@@ -5,17 +19,19 @@ import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 import com.liferay.saml.admin.rest.dto.v1_0.IdpConnection;
 import com.liferay.saml.persistence.model.SamlSpIdpConnection;
 import com.liferay.saml.persistence.service.SamlSpIdpConnectionLocalService;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+/**
+ * @author Stian Sigvartsen
+ */
 @Component(
-	//enabled = false,
 	property = "dto.class.name=com.liferay.saml.persistence.model.SamlSpIdpConnection",
-	service = {IdpConnectionDTOConverter.class, DTOConverter.class}
+	service = {DTOConverter.class, IdpConnectionDTOConverter.class}
 )
 public class IdpConnectionDTOConverter
 	implements DTOConverter<SamlSpIdpConnection, IdpConnection> {
-
 
 	@Override
 	public String getContentType() {
@@ -23,7 +39,8 @@ public class IdpConnectionDTOConverter
 	}
 
 	@Override
-	public IdpConnection toDTO(DTOConverterContext dtoConverterContext) throws Exception {
+	public IdpConnection toDTO(DTOConverterContext dtoConverterContext)
+		throws Exception {
 
 		SamlSpIdpConnection samlSpIdpConnection =
 			_samlSpIdpConnectionLocalService.getSamlSpIdpConnection(
@@ -31,17 +48,18 @@ public class IdpConnectionDTOConverter
 
 		return new IdpConnection() {
 			{
-				enabled = samlSpIdpConnection.isEnabled();
-				entityId = samlSpIdpConnection.getSamlIdpEntityId();
-				id = samlSpIdpConnection.getSamlSpIdpConnectionId();
-				metadataUrl = samlSpIdpConnection.getMetadataUrl();
-				name = samlSpIdpConnection.getName();
-				nameIdFormat = samlSpIdpConnection.getNameIdFormat();
 				assertionSignatureRequired =
 					samlSpIdpConnection.isAssertionSignatureRequired();
 				clockSkew = samlSpIdpConnection.getClockSkew();
+				enabled = samlSpIdpConnection.isEnabled();
+				entityId = samlSpIdpConnection.getSamlIdpEntityId();
 				forceAuthn = samlSpIdpConnection.isForceAuthn();
-				samlSpIdpConnection.getMetadataUpdatedDate();
+				id = samlSpIdpConnection.getSamlSpIdpConnectionId();
+				metadataUpdatedDate =
+					samlSpIdpConnection.getMetadataUpdatedDate();
+				metadataUrl = samlSpIdpConnection.getMetadataUrl();
+				name = samlSpIdpConnection.getName();
+				nameIdFormat = samlSpIdpConnection.getNameIdFormat();
 				signAuthnRequest = samlSpIdpConnection.isSignAuthnRequest();
 				unknownUsersAreStrangers =
 					samlSpIdpConnection.isUnknownUsersAreStrangers();
@@ -53,4 +71,5 @@ public class IdpConnectionDTOConverter
 
 	@Reference
 	private SamlSpIdpConnectionLocalService _samlSpIdpConnectionLocalService;
+
 }
