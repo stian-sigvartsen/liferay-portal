a46ce49fd414c67a4c7500927b0495c3ab45e276
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2022-06-19 15:23:34 +0200

    LPS-120925 testPostSamlProvider()

--- modules/dxp/apps/saml/saml-admin-rest-test/src/testIntegration/java/com/liferay/saml/admin/rest/resource/v1_0/test/SamlProviderResourceTest.java
+++ modules/dxp/apps/saml/saml-admin-rest-test/src/testIntegration/java/com/liferay/saml/admin/rest/resource/v1_0/test/SamlProviderResourceTest.java
@@ -19,6 +19,7 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.configuration.test.util.ConfigurationTemporarySwapper;
 import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -26,6 +27,7 @@ import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
@@ -33,7 +35,11 @@ import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.security.service.access.policy.model.SAPEntry;
 import com.liferay.portal.security.service.access.policy.service.SAPEntryLocalService;
 import com.liferay.portal.test.rule.Inject;
+import com.liferay.saml.admin.rest.client.dto.v1_0.Idp;
+import com.liferay.saml.admin.rest.client.dto.v1_0.IdpConnection;
 import com.liferay.saml.admin.rest.client.dto.v1_0.SamlProvider;
+import com.liferay.saml.admin.rest.client.dto.v1_0.Sp;
+import com.liferay.saml.admin.rest.client.dto.v1_0.SpConnection;
 import com.liferay.saml.admin.rest.client.http.HttpInvoker;
 import com.liferay.saml.runtime.configuration.SamlProviderConfiguration;
 import com.liferay.saml.util.PortletPropsKeys;
@@ -111,6 +117,18 @@ public class SamlProviderResourceTest extends BaseSamlProviderResourceTestCase {
 	@After
 	public void tearDown() {
 		_cleanUp();
+	}
+
+	private void _addSAPEntry() throws PortalException {
+		_sapEntry = _sapEntryLocalService.addSAPEntry(
+			TestPropsValues.getUserId(),
+			"com.liferay.saml.admin.rest.internal.resource.v1_0." +
+			"SamlProviderResourceImpl#*",
+			true, true, "Guest",
+			HashMapBuilder.put(
+				LocaleUtil.getDefault(), "Guest"
+			).build(),
+			ServiceContextTestUtil.getServiceContext());
 	}
 
 	private void _cleanUp() {
@@ -250,7 +268,7 @@ public class SamlProviderResourceTest extends BaseSamlProviderResourceTestCase {
 
 			Object companyId = properties.get("companyId");
 
-			if (companyId.equals(this.testCompany)) {
+			if (companyId.equals(testCompany)) {
 				testCompanyConfigurationDictionary = properties;
 				testCompanyConfiguration = configuration;
 
@@ -346,15 +364,7 @@ public class SamlProviderResourceTest extends BaseSamlProviderResourceTestCase {
 
 	public void testPatchSamlProvider() throws Exception {
 
-		_sapEntry = _sapEntryLocalService.addSAPEntry(
-			TestPropsValues.getUserId(),
-			"com.liferay.saml.admin.rest.internal.resource.v1_0." +
-			"SamlProviderResourceImpl#*",
-			true, true, "Guest",
-			HashMapBuilder.put(
-				LocaleUtil.getDefault(), "Guest"
-			).build(),
-			ServiceContextTestUtil.getServiceContext());
+		_addSAPEntry();
 
 		SamlProvider defaultSamlProvider = samlProviderResource.getSamlProvider();
 
@@ -414,13 +424,11 @@ public class SamlProviderResourceTest extends BaseSamlProviderResourceTestCase {
 
 	public void testPostSamlProvider() throws Exception {
 
-		if (1 == 1) {
-			return;
-		}
+		_addSAPEntry();
 
-		SamlProvider samlProvider = new SamlProvider() {
+		SamlProvider postSamlProvider = new SamlProvider() {
 			{
-				enabled = true;
+				enabled = false;
 				entityId = "";
 				signMetadata = false;
 				sslRequired = false;
@@ -429,41 +437,72 @@ public class SamlProviderResourceTest extends BaseSamlProviderResourceTestCase {
 
 		String role = SamlProvider.Role.SP.getValue();
 
-		SamlProvider samlProvider2 =
-			samlProviderResource.postSamlProvider(samlProvider);
+		postSamlProvider.setSp(new Sp() {
+			{
+				allowShowingTheLoginPortlet = false;
+				assertionSignatureRequired = false;
+				clockSkew = 1000l;
+				ldapImportEnabled = false;
+				signAuthnRequest = false;
+			}
+		});
 
-//		Assert.assertEquals(
-//			Response.Status.BAD_REQUEST.getStatusCode(),
-//			httpResponse.getStatusCode());
+		SamlProvider samlProvider =
+			samlProviderResource.postSamlProvider(postSamlProvider);
 
+		_addExpectedEnrichment(postSamlProvider);
 
-//		SamlProvider randomSamlProvider = randomSamlProvider();
-//
-//		SamlProvider postSamlProvider = testPostSamlProvider_addSamlProvider(
-//			randomSamlProvider);
-//
-//		assertEquals(randomSamlProvider, postSamlProvider);
-//		assertValid(postSamlProvider);
+		Assert.assertEquals(postSamlProvider, samlProvider);
 
+		postSamlProvider.setIdp(new Idp() {
+			{
+				authnRequestSignatureRequired = false;
+				defaultAssertionLifetime = 10000;
+				sessionMaximumAge = 60000l;
+				sessionTimeout = 60000l;
+			}
+		});
 
-//		SamlProvider samlProvider = new SamlProvider() {
-//			{
-//				enabled = true;
-//				entityId = "";
-//				signMetadata = false;
-//				sslRequired = false;
-//			}
-//		};
-//
-//		String role = SamlProvider.Role.SP.getValue();
-//
-//		SamlProvider samlProvider2 =
-//			samlProviderResource.postSamlProvider(samlProvider);
-//
-//		Assert.assertEquals(
-//			Response.Status.BAD_REQUEST.getStatusCode(),
-//			httpResponse.getStatusCode());
+		HttpInvoker.HttpResponse httpResponse =
+			samlProviderResource.postSamlProviderHttpResponse(postSamlProvider);
 
+		Assert.assertEquals(
+			Response.Status.BAD_REQUEST.getStatusCode(),
+			httpResponse.getStatusCode());
+
+		JSONObject jsonObject =
+			JSONFactoryUtil.createJSONObject(httpResponse.getContent());
+
+		Assert.assertEquals(
+			"Can only configure one of sp & idp roles",
+			jsonObject.get("title"));
+
+		postSamlProvider.setSp((Sp)null);
+
+		samlProvider = samlProviderResource.postSamlProvider(postSamlProvider);
+
+		_addExpectedEnrichment(postSamlProvider);
+
+		Assert.assertEquals(postSamlProvider, samlProvider);
+	}
+
+	private void _addExpectedEnrichment(SamlProvider samlProvider) {
+		if (samlProvider.getIdp() != null) {
+			Idp idp = samlProvider.getIdp();
+
+			if (idp.getSpConnections() == null) {
+				idp.setSpConnections(new SpConnection[0]);
+			}
+			samlProvider.setRole(SamlProvider.Role.IDP);
+		}
+		else if (samlProvider.getSp() != null) {
+			Sp sp = samlProvider.getSp();
+
+			if (sp.getIdpConnections() == null) {
+				sp.setIdpConnections(new IdpConnection[0]);
+			}
+			samlProvider.setRole(SamlProvider.Role.SP);
+		}
 	}
 
 	@Override
